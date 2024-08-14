fae7de1ab52def85faba6bcab1c10b41e759ab1c
collins-hue <collinsmurgorqw@gmail.com>
2024-08-14 22:38:08 +0300

    Worked on wallpaper page and download button

--- wallpaper_app/urls.py
+++ wallpaper_app/urls.py
@@ -7,8 +7,9 @@ app_name = 'wallpaper_app'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('wallpaper/<int:id>/', views.wallpaper_info, name='wallpaper_info'),
     path('about_us/', views.about_us, name='about_us'),
     path('terms/', views.terms_condition, name='terms_condition'),
     path('contact/', views.contact_us, name='contact_us'),
-    path('<int:id>/', views.wallpaper_info, name='wallpaper_info'),
+
 ]
--- wallpaper_app/views.py
+++ wallpaper_app/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404, HttpResponse
 from django.shortcuts import render, get_object_or_404
 
 from wallpaper_app.models import About, TermsConditions, WallpaperInfo
@@ -8,8 +9,19 @@ def home(request):
     return render(request, 'index.html', {"all_wallpapers": all_wallpapers})
 
 def wallpaper_info(request, id):
-    obj = get_object_or_404(WallpaperInfo, id=id)
-    return render(request, 'wallpaper.html', {"object": obj})
+    wallpaper = get_object_or_404(WallpaperInfo, id=id)
+    
+    if request.GET.get('download'):
+        file_path = wallpaper.image.path
+        try:
+            with open(file_path, 'rb') as file:
+                response = HttpResponse(file.read(), content_type='application/force-download')
+                response['Content-Disposition'] = f'attachment; filename="{wallpaper.image.name}"'
+                return response
+        except FileNotFoundError:
+            raise Http404("File not found")
+    return render(request, 'wallpaper.html', {'wallpaper': wallpaper})
+
 
 def about_us(request):
     about = About.objects.all()
